4c764fd7a98a903a1dcaac59a31b2fc81f772392
Timofejs Žaruns <90261429+Tim-Zaruns@users.noreply.github.com>
2021-12-02 17:36:42 +0200

    feat: add completed todo

--- src/pages/ToDoes.tsx
+++ src/pages/ToDoes.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import { Progress } from 'antd';
 import Input from '../components/Input';
 import Checkbox from '../components/checkbox';
 import './todo.scss';
+import 'antd/dist/antd.css';
 
 type TodoType = {
   title: string,
   complete: boolean,
   edit: boolean,
-  tags: {
-    today: boolean,
-    thisWeek: boolean,
-    thisMonth: boolean
-  }
+  tags: string;
 };
 
 const ToDoes = () => {
   const [inputValue, setInputValue] = useState('');
   const [toDoList, setToDoList] = useState<TodoType[]>([]);
   const [completedTodoList, setCompletedTodoList] = useState<boolean>(false);
-  const [editedInputValue, setEditedInputValue] = useState('');
+  const [editedTaskInputValue, setEditedTaskInputValue] = useState('');
   const [tagValue, setTagValue] = useState('');
+  const [search, setSearch] = useState('');
 
-  const clickHandler = () => {
+  const addNewTask = () => {
     setToDoList([...toDoList, {
-      title: inputValue, complete: false, edit: false, tags: { today: false, thisMonth: false, thisWeek: false },
+      title: inputValue, complete: false, edit: false, tags: '',
     }]);
     setInputValue('');
   };
 
-  const removeHandler = (index: number) => {
+  useEffect(() => {
+    const Tasks = localStorage.getItem('Tasks');
+    if (Tasks) {
+      setToDoList(JSON.parse(Tasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('Tasks', JSON.stringify(toDoList));
+  }, [toDoList]);
+
+  const removeCurrentTask = (index: number) => {
     const filteredToDos = toDoList.filter((_, i) => (
       i !== index
     ));
@@ -49,11 +60,13 @@ const ToDoes = () => {
 
   const changeTaskInfo = (index: number) => {
     const clonedList = [...toDoList];
-    clonedList[index].title = editedInputValue;
+    clonedList[index].title = editedTaskInputValue;
+    clonedList[index].edit = !clonedList[index].edit;
+    clonedList[index].tags = tagValue;
+    setTagValue('');
     setToDoList(clonedList);
-    setEditedInputValue('');
+    setEditedTaskInputValue('');
   };
-
   return (
     <div>
       <span>show Completed Tasks</span>
@@ -64,39 +77,63 @@ const ToDoes = () => {
       <Input
         inputValue={inputValue}
         onChange={setInputValue}
+        placeholder="...Add new task"
       />
       <button
-        onClick={clickHandler}
+        onClick={addNewTask}
         className="button"
       >
         ADD
       </button>
+      <button onClick={() => setToDoList([])}>
+        Clear All
+      </button>
+      <div>
+        <Input inputValue={search} onChange={setSearch} placeholder="...Search by tag" />
+      </div>
       <div className="todo__wrapper">
-        {toDoList.filter((list) => list.complete || !completedTodoList)
+        <div>
+          <Progress
+            percent={Math.floor((100 / toDoList.length) * toDoList.filter((list) => list.complete).length)}
+            size="small"
+          />
+        </div>
+        {toDoList
+          .filter((tagName) => (tagName.tags.toLowerCase().includes(search.toLowerCase())))
+          .filter((list) => list.complete || !completedTodoList)
           .map((item, index) => (
             <div key={item.title} className="todo">
               <div>
                 {!item.edit ? item.title
                   : (
                     <div>
-                      <Input inputValue={editedInputValue} onChange={setEditedInputValue} />
-                      <button onClick={() => changeTaskInfo(index)}>Edit</button>
-                      <select value={tagValue} onChange={(e) => setTagValue(e.target.value)}>
-                        <option value="thisMonth">This Month</option>
+                      <Input
+                        inputValue={editedTaskInputValue}
+                        onChange={setEditedTaskInputValue}
+                        placeholder="...Edit current Task"
+                      />
+                      <button onClick={() => changeTaskInfo(index)}>Save Changes</button>
+                      <select
+                        value={tagValue}
+                        onChange={(e) => setTagValue(e.target.value)}
+                      >
+                        <option value="">none</option>
+                        <option value="month">Month</option>
                         <option value="today">Today</option>
-                        <option value="thisWeek">This Week</option>
+                        <option value="week">Week</option>
                       </select>
                     </div>
                   )}
               </div>
               <div className="todo__completed">
+                <span>{item.tags ? `#${item.tags}` : ''}</span>
                 <span>completed</span>
                 <Checkbox
                   checkboxValue={item.complete}
                   onChange={() => toggleCompletedTask(index)}
                 />
                 <button
-                  onClick={() => removeHandler(index)}
+                  onClick={() => removeCurrentTask(index)}
                 >
                   x
                 </button>
